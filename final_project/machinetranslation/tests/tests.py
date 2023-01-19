640fe8e3516a3a86996bf5ca6b19344e11e87d55
bashgame <amen.kester@gmail.com>
2023-01-18 22:29:41 -0500

    Had to change unit tests to try to comply with rubric
    Reran tests and took a new screenshot
    
    Screenshot still doesn't show the test names just that all passed

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -3,18 +3,30 @@ import unittest
 from translator import english_to_french, french_to_english
 
 class TestEnglishToFrench(unittest.TestCase):
-    def test1(self):
+    def test_Hello(self):
         self.assertEqual(english_to_french("Hello"), "Bonjour")
+
+    def test_notTranslated(self):
         self.assertNotEqual(english_to_french("Hello"), "Hello")
+
+    def test_Goodbye(self):
         self.assertEqual(english_to_french("Goodbye"), "Au revoir")
+
+    def test_null_strings(self):
         self.assertEqual(english_to_french(), "The supplied string is empty")
         self.assertEqual(english_to_french(''), "The supplied string is empty")
 
 class TestFrenchToEnglish(unittest.TestCase):
-    def test2(self):
+    def test_Hello(self):
         self.assertEqual(french_to_english("Bonjour"), "Hello")
+        
+    def test_notTranslated(self):
         self.assertNotEqual(french_to_english("Bonjour"), "Bonjour")
+        
+    def test_Goodbye(self):
         self.assertEqual(french_to_english("Au revoir"), "Goodbye")
+        
+    def test_null_strings(self):
         self.assertEqual(french_to_english(), "The supplied string is empty")
         self.assertEqual(french_to_english(''), "The supplied string is empty")
 
